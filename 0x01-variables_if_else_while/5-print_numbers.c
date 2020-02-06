b9bafa453239663f11e87e42b7229637dc1fb1d7
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-06 20:01:52 +0000

    counts from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* main - function that prints all single digit numbers of base 10 starting from 0
+* Return: Always 0 Success
+*/
+
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%d", x);
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
