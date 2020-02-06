8409ac10b531929456ea1d6de951245f3e121d0f
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-06 18:46:35 +0000

    appeased betty-style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,30 +2,28 @@
 #include <time.h>
 #include <stdio.h>
 
-
 /**
 * main - Calculates a random number, n
-* elif statements print whether or not the last digit of n is > 5 || == 0 || < 6 !0
+* elif statements print whether or not the last # of n is > 5 || == 0 || < 6 !0
 * Return: Always 0 Success
 */
- 
+
 int main(void)
 {
 	int n;
 	int last;
 	char lessSixNotZero[30] = "and is less than 6 and not 0";
 	char isZero[10] = "and is 0";
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (last > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5", n, last);
 	}
 	else if (last < 6 && last != 0)
 	{
-		printf("Last digit of %d is %d %s", n, last, lessSixNotZero); 
+		printf("Last digit of %d is %d %s", n, last, lessSixNotZero);
 	}
 	else
 	{
