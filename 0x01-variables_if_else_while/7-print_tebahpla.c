eb691b5874bcd6cdc3d44f09035a7e8598797fa6
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-06 21:28:26 +0000

    a-z in reverse using putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* main - prints a-z in reverse using putchar
+* Return: Always 0 Success
+*/
+
+int main(void)
+{
+	int x;
+	for (x ='z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
