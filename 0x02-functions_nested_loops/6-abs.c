c7f6e9a90dd6c21702d0e29235723e5f89d5303c
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-11 18:39:10 +0000

    calculates the abs of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton'.h"
+
+/**
+ * _abs(int) - function that computes the absolute value of and integer
+ * @int: integer input by user
+ * Return: (void)
+ */
+
+_abs(int)
+{
+	if (int > 0)
+	{
+		return (int);
+	}
+	else
+	{
+		return (int * -1);
+	}
+}
