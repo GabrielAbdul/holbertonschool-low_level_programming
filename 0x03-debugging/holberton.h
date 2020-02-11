a9ad9a712f304e1841df71ea33f9de82493fafcf
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-11 18:26:15 +0000

    removed code for func in holberton.h

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -5,23 +5,5 @@
 
 void positive_or_negative(int i);
 
-int largest_number(int a, int b, int c)
-{
-    int largest;
-
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (b > a && b > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-
-    return (largest);
-}
+int largest_number(int a, int b, int c);
 #endif
