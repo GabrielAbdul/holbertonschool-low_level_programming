ae4d36d8d19767792c8844262b9e5feb860e7be2
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-12 05:14:55 +0000

    added descriptions for x and y

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,9 +2,9 @@
 
 /**
  * add - adds two integers and returns the result.
- *
+ * @x: first integer input by user
+ * @y: second integer input by user
  * Return: (int)
- *
  */
 
 int add(int x, int y)
