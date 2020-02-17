99e9379d2f62f37452f1433d47f2928694c6c0f9
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-17 16:09:13 +0000

    initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/*
+ * reset_to_98 - functino that takes a pointer to an int
+ * as a parameter and updates the value it points to to 98
+ *
+ * Return: (void)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,6 @@
+#ifndef
+#define HOLBERTON_H
+void reset_to_98(int *n);
+
+
+#endif
