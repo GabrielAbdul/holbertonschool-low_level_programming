12833769577f7432d14c88fbb3d60f940491ac79
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-17 23:01:44 +0000

    adds all mandatory tasks, some incomplete

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - function that prints n elements of an array of integers
+ * @n: number of elements in the array to be printed
+ * @a: name of array
+ * Return: (void)
+ */
+
+void puts_half(int *a, int n)
+{
+	int i;
+	int arrayLength;
+
+	arrayLength = sizeof(a) / sizeof(a[0]);
+
+	for (i = 0; i <= arrayLength; i++)
+		printf(
+}
+
+	
+
+
