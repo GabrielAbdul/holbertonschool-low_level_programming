c5a262824902003910134c337b5130656bee3beb
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-20 14:42:20 +0000

    push of updated holberton.h and function skeleton for 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - function that compares two strings..
+ * @s1: string that will get src copied to it
+ * @s2: string to be copied
+ * Return: Ascii value of the difference of s1 and s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	
+}
