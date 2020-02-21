8a30610e0946de8241903b58f27ce6016dd755d2
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-21 03:01:05 +0000

    change fprintf to write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,9 @@
 * Return: Always 0 (Success)
 */
 
+
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return(1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-1\n", 59);
+	return (1);
 }
