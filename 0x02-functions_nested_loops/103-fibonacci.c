78b76f607cc80e18731fe1fe4896d96f4ec69d99
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-23 12:32:53 +0000

    prints even fibonacci numbers whose value do not exceed 4 million

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints the first 50 fibonacci numbers
+ * starting with 1 and 2, followed by a new line
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+ /*  fibonacci sequence 1, 2, 3, 5, 8, 13, 21... */
+
+	unsigned long firstNum;
+	unsigned long secondNum;
+	int count;
+	unsigned long temp;
+
+	firstNum = 1;
+	secondNum = 2;
+
+	printf("%ld, %ld", firstNum, secondNum);
+
+	for (count = 0; count < 30; count++)
+	{
+		temp = firstNum + secondNum;
+		firstNum = secondNum;
+		secondNum = temp;
+		if (temp % 2 == 0)
+			printf(", %ld", temp);
+	}
+	printf("\n");
+
+	return (0);
+}
