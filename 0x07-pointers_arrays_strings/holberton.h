a411b512d92cb76bed98fb659280ea4d9c37b882
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-24 14:25:20 +0000

    initial commit

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+int _putchar(char c);
+
+char *_memset(char *s, char b, unsigned int n);
+
+void simple_print_buffer(char *buffer, unsigned int size);
+#endif
