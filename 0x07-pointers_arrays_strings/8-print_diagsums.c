5682362f91ae4833812cfee4547dc5a885e59321
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-25 03:15:32 +0000

    pushes final tasks and placeholder file for 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_diagsums - function that prints the sum of the
+ * two diagonals of a square matrix of integers
+ * @size: size
+ * @a: array
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+
+}
