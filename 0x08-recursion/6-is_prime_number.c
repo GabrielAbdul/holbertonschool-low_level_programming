647bdfce4ffc2fec0abead968cc5116b8be20859
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-26 14:35:40 +0000

    function that finds if number is prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - function that returns 1 if n is a prime number
+ * otherwise return 0
+ *
+ * @n: integer test prime
+ * Return: int
+ */
+
+int is_prime_number(int n)
+{
+	int i = 2, flag = 0;
+
+	for (; i <= n / 2; i++)
+	{
+		if (n % i == 0)
+		{
+			flag = 1;
+			break;
+		}
+	}
+	if (n == 1 || n < 0)
+		return 0;
+	else
+	{
+		if (flag == 0)
+			return (1);
+		else
+			return (0);
+	}
+	return (0);
+}
