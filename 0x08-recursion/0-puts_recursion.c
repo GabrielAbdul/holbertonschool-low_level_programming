160ee50ea4bee0fab6e339b41c4d3712fc76ebec
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-26 12:20:06 +0000

    function that prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by a newline
+ * 
+ * @s: input string to print
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		_putchar(s[i]);
+	_putchar('\n');
+}
