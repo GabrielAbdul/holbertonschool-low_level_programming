4ad8c3d1a43fe86fc447c3dabd3ce4fa1e6a77fe
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-26 18:06:05 +0000

    added () around return

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _pow_recursion - function that returns the value of x rasied to pow of y
- * 
+ *
  * @x: input integer going to be raised by y
  * @y: input integer to raise x by
  * Return: int
@@ -11,9 +11,9 @@
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return -1;
+		return (-1);
 	if (y == 0)
-		return 1;
+		return (1);
 	x *=  _pow_recursion(x, y - 1);
 	return (x);
 }
