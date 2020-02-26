ac8547290bbce7c92a202eb168b7c44d0322eb5c
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-26 18:04:45 +0000

    added parenthesis around return

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,7 +2,7 @@
 
 /**
  * factorial - function that returns the factorial of a given number
- * 
+ *
  * @n: input integer to find factorial of
  *
  * Return: int
@@ -11,11 +11,11 @@
 int factorial(int n)
 {
 	if (n < 0)
-		return -1;
+		return (-1);
 	if (n == 0 || n == 1)
-		return 1;
+		return (1);
 
 	n = n * factorial(n - 1);
 
-	return n;
+	return (n);
 }
