d01541b9a73cea24ba15a8d1f040cfedc6f39620
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-26 14:02:22 +0000

    template for number 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a num
+ * @n: integer to find square root of
+ * Return: int
+ */
+
+int _sqrt_recursion(int n);
+{
+
+}
