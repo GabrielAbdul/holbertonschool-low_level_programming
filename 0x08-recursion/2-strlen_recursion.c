eac1af02f0c6791b2caa3585c31e3550ab9a0ff7
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-26 19:40:16 +0000

    fixed pointer asterisk

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen_recursion - function that returns the length of a string.
- * 
+ *
  * @s: input string to find length
  *
  * Return: int
@@ -10,7 +10,7 @@
 
 int _strlen_recursion(char *s)
 {
-	if (s* == '\0')
+	if (*s == '\0')
 		return;
 	return (1 + _strlen_recursion(s + 1));
 }
