0fc8e934df161f771abac853ba6c9a41d43b61e7
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-26 23:58:11 +0000

    function that computes square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,7 +6,29 @@
  * Return: int
  */
 
-int _sqrt_recursion(int n);
+int _sqrt_recursion(int n)
 {
-	
+	int sqr;
+
+	sqr = _pow(1, n);
+	return (sqr);
+}
+
+/**
+ * _pow - checks whether the square of a number
+ * @x: will be square, incremented
+ * @y: limiter
+ * Return: int
+ */
+
+int _pow(int x, int y)
+{
+	if ((x * x) < y)
+		return (_pow((x + 1), y));
+	if ((x * x) == y)
+		return (x);
+	if ((x * x) > y)
+		return (-1);
+
+	return (x);
 }
