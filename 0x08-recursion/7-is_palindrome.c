40bd8b41bbcf7d7ccfa37ccb9bc5ad224f1dbb85
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-02-27 02:04:57 +0000

    fixed syntax

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -13,12 +13,12 @@ int is_palindrome(char *s)
 	int len;
 	int i = 1;
 
-	len = _strlen(s);
+	len = _strlen_recursion(s);
 
 	if (*s != s[len - 1])
 		return (0);
 	if (*s == s[len - i--])
-		is_palindrome(*(s + i));
+		is_palindrome(s + 1);
 
 	return (1);
 }
