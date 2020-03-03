94bca07bb6be8e92de635d038c2a253f408f9a79
cmm-user <gabrielabdul1@gmail.com>
2020-03-03 18:45:06 +0000

    removed malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -22,11 +22,6 @@ char *create_array(unsigned int size, char c)
 
 	p = (char *)malloc(sizeof(char) * size);
 
-	if ((char *)malloc(sizeof(char) * size))
-	{
-		return (NULL);
-	}
-
 	for (i = 0; i < size; i++)
 	{
 		*(p + i) = c;
