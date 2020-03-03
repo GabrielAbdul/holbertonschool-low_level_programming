1557e16d23f4f0bfdac00656d0ad48cb4d187bc2
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-03-03 16:49:27 +0000

    updated header file, function skeleton for 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function that returns a 2d array of integers
+ *
+ * @width: input array collums
+ * @height: input array rows
+ *
+ * Return: pointer to a 2d array of integers
+ */
+
+char **alloc_grid(int width, int height)
+{
+	int **p;
+}
