3acbee6a79337be1313e22533c25d120dd6b601a
cmm-user <gabrielabdul1@gmail.com>
2020-03-03 21:55:16 +0000

    got rid of infinite loops

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,10 @@ char *str_concat(char *s1, char *s2)
 	int s1len = 0, s2len = 0, i, j;
 	int catlen;
 
-	while (*s1)
+	while (s1[s1len])
 		s1len++;
-	while (*s2)
+	
+	while (s2[s2len])
 		s2len++;
 
 	if (s1 == NULL)
