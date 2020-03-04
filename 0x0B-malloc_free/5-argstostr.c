f15fe27f3c2940801a68746b3e9cc0dcefb43dd3
cmm-user <gabrielabdul1@gmail.com>
2020-03-04 21:56:08 +0000

    made betty compliant

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -3,45 +3,45 @@
 #include "holberton.h"
 
 /**
- * * argstostr - concatenates all the program's arguments into a string
- * *
- * * @ac: argument count
- * * @av: argument vector
- * *
- * * Return: char pointer to string, or NULL if malloc fails, or if ac or av = 0
- * */
+ *  * argstostr - concatenates all the program's arguments into a string
+ *   *
+ *    * @ac: argument count
+ *     * @av: argument vector
+ *      *
+ *       * Return: char pointer to string, or NULL if malloc fails, or if ac or av = 0
+ *        **/
 
 char *argstostr(int ac, char **av)
 {
-        char *array;
-        int z = 0;
-        int i, len;
-        int size = 1;
+	char *array;
+	int z = 0;
+	int i, len;
+	int size = 1;
 
-        if (ac == 0 || av == NULL)
-                return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-        for (i = 0; i < ac; i++)
-                for (len = 0; av[i][len]; len++)
-                        size++;
+	for (i = 0; i < ac; i++)
+		for (len = 0; av[i][len]; len++)
+			size++;
 
-        array = (char *)malloc(size * sizeof(char));
+	array = (char *)malloc(size * sizeof(char));
 
-        if (array == NULL)
-                return (NULL);
-
-        for (i = 0; i < ac; i++)
-        {
-                for (len = 0; av[i][len]; len++)
-                {
-                        array[z] = av[i][len];
-                        z++;
-                }
-                array[z] = '\n';
-		z++;
-        }
-        array[z] = '\0';
 	if (array == NULL)
 		return (NULL);
-        return (array);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (len = 0; av[i][len]; len++)
+		{
+			array[z] = av[i][len];
+			z++;
+		}
+		array[z] = '\n';
+		z++;
+	}
+	array[z] = '\0';
+	if (array == NULL)
+		return (NULL);
+	return (array);
 }
