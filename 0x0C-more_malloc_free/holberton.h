bd4365f3dd7234bfe8c7505552b0b14308b8b8c3
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-03-05 18:08:30 +0000

    updated header file with prototypes for ex 1

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,5 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 void *malloc_checked(unsigned int b);
-
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+int len(char *string);
+char *str_concat(char *s1, char *s2);
 #endif
