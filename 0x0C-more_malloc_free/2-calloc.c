7030f921319334ff9508dfdc7a3f58cc7e50f588
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-03-05 20:14:45 +0000

    initial commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - allocates memory for an array of nmemb elements of 
+ *
+ * @nmemb: # of elements in array
+ * @size: size in bytes of array
+ *
+ * Return: array
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	memset(p, 0, nmemb * size);
+	return (p);
+}
