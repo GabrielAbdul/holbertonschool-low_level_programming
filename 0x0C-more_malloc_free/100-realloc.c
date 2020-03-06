1b8076010d92a8301ab4901d941ced6d341faf8d
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-03-06 06:01:09 +0000

    betty fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,7 +7,7 @@
  *
  * @ptr: ptr
  * @old_size: old size
- * new_size: new size
+ * @new_size: new size
  *
  * Return: (p);
  */
@@ -29,10 +29,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr)
-		return(NULL);
+		return (NULL);
 	}
 	p = malloc(new_size);
 	free(ptr);
 
-	return(p);
+	return (p);
 }
