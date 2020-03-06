d71971b204ad2832f48f35d2fbd6bc3ad35f55da
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-03-06 06:05:29 +0000

    compile fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,19 +16,20 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *p;
 
+	if (ptr == NULL)
+	{
+		free(ptr);
+		p = malloc(new_size);
+		return (p);
+	}
+	
 	if (new_size == old_size)
 	{
 		return (ptr);
 	}
-	if (ptr == NULL)
-	{
-		free(old_size);
-		p = malloc(new_size);
-		return (p);
-	}
 	if (new_size == 0 && ptr != NULL)
 	{
-		free(ptr)
+		free(ptr);
 		return (NULL);
 	}
 	p = malloc(new_size);
