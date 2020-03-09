a22d05dd41586a4ed232f7f81782fa7c80e82314
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-03-09 15:58:14 +0000

    initial commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program that prints the name of the file
+ * it was compiled from
+ *
+ * Return: On success 0
+ *
+ */
+
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
