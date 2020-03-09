c5fff42e6b1a606d8d8c0ab4a2e5f5bd0812209b
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-03-09 19:15:30 +0000

    accounted for NULL

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - function that initializes a variable of
@@ -13,6 +14,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return (NULL);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
