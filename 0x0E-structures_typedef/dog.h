dc7e18c97e39f1bc8292e94976b4ab7c62952fca
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-03-09 19:07:31 +0000

    initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct of a dog
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs (hopefully adopted) owner
+ *
+ */
+
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+#endif /* DOG_H */
