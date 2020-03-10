b950356cf2c0bdd03b2b58da99c827b85eaad0f2
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-03-10 03:42:02 +0000

    added void pointer to indicate end of struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,7 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
+	void *p;
 };
 
 typedef struct dog dog_t;
@@ -22,4 +23,5 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif /* DOG_H */
