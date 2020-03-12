5a19a4293f8acf90a4c3e6f8d9fef910816ea65c
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-03-12 02:23:45 +0000

    initial commit

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - program that prints the opcodes of its own main function
+ *
+ * @argc: number of args
+ * @argv: string array of args
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	return (0);
+}
