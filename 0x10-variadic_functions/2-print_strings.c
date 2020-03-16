76e88c038c4f9a947b44e5cfa513667a70dd6a6f
GabrielAbdul <gabrielabdul1@gmail.com>
2020-03-16 02:57:57 +0000

    changed inequality to less than

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -31,10 +31,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 			printf("%s", p);
 
-			if (i != n - 1)
+			if (i < n - 1)
 			{
+
 				printf("%s", separator);
 			}
+
 		}
 	printf("\n");
 	}
