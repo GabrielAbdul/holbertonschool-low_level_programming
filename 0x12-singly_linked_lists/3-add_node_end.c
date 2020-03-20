811de09019e4557dc1863fde1e0966f9bf6c1a56
GabrielAbdul <gabrielabdul1@gmail.com>
2020-03-20 13:44:52 +0000

    Closer to correct output

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,4 +1,4 @@
-  nclude "lists.h"
+#include "lists.h"
 #include <string.h>
 #include <stdlib.h>
 
@@ -10,17 +10,29 @@
  *
  * Return: adress of new node
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *node_added = malloc(sizeof(list_t));
+	list_t *checker;
 
-	if (!head)
+	if (!node_added)
 		return (NULL);
+	if (*head == NULL)
+	{
+		*head = node_added;
+		node_added->next = NULL;
+		return (*head);
+	}
+	
+	checker = *head;
 
+	while (checker->next)
+		checker = checker->next;
+
+	checker->next = node_added;
 	node_added->str = strdup(str);
 	node_added->len = _strlen(str);
-	node_added->next = *head;
-	*head = node_added;
+	node_added->next = NULL;
 
 	return (*head);
 	
