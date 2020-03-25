047b7f1760d13abbe8e586f649acf50dba58d7e1
gabrielabdul <gabrielabdul1@gmail.com>
2020-03-25 02:12:00 +0000

    changed while condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	else
 	{
 		node = (*head);
-		while (node)
+		while (count < idx)
 		{
 			if (!node->next)
 				return (NULL);
