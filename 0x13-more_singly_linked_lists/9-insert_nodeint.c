bcc288dd4f14dd5a07e5bc12fe11ec85f329b661
gabrielabdul <gabrielabdul1@gmail.com>
2020-03-25 02:07:52 +0000

    added break statement

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -42,6 +42,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				prev_node->next = node_to_add;
 				node_to_add->n = n;
 				node_to_add->next = node;
+				break;
 			}
 		}
 	}
