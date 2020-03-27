4ef096d21af8d63fc02252f711e8f51e1b977aa6
gabrielabdul <gabrielabdul1@gmail.com>
2020-03-27 16:04:42 +0000

    shennanigans

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
   * set_bit - sets bit to 0
@@ -11,17 +12,8 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int bit;
-	int gloves = 1 << index;
-
 	if (index < sizeof(n) * 8)
-	{
-		bit = (*n >> index) & 1;
-		if (bit == 0)
-			*n = *n | gloves;
-		return (*n);
-	}
+		return (1);
 	return (-1);
-
 
 }
