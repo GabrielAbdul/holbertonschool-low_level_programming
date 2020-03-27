06f49b656f27d2ab892a33278b542d1cf5a67394
gabrielabdul <gabrielabdul1@gmail.com>
2020-03-27 01:17:15 +0000

    initial commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  *
+  *
+  *
+  *
+  *
+  */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int len = 0;
+
+	if (!b)
+		return (0);
+	while (b[len])
+	{
+		if (b[len] == '1' || b[len] == '0')
+		{
+			result <<= 1;
+			if (b[len] == '1')
+				result++;
+		}
+		else
+			return (0);
+		len++;
+	}
+	return (result);
+}
