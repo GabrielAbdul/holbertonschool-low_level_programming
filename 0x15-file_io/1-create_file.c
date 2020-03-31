45692cf08f47c70ee7fbf36891af94b1172765f2
GabrielAbdul <gabrielabdul1@gmail.com>
2020-03-31 18:51:37 +0000

    compares write_count to failure of write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,6 +12,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int len = 0;
+	int write_count;
 
 	if (!filename)
 		return (-1);
@@ -22,9 +23,9 @@ int create_file(const char *filename, char *text_content)
 	for (; text_content[len]; len++)
 		continue;
 	if (text_content)
-		write(fd, text_content, len);
-	if (!text_content)
-		write(fd, "", 0);
+		write_count = write(fd, text_content, len);
+	if (write_count == -1)
+		return (write_count); 
 	close(fd);
 	return (1);
 }
