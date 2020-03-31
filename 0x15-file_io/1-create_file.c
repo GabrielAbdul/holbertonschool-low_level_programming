cf6a02eb8af9b9c52c99e2cd364eb093bf73dde6
gabrielabdul <gabrielabdul1@gmail.com>
2020-03-31 17:50:35 +0000

    initial commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+  * create_file - function that creates a file
+  *
+  * @filename: file to create
+  * @text_content: text to fill file with
+  *
+  * Return: 1 (on success)
+  */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (!fd)
+		return (-1);
+	for (; text_content[len]; len++)
+		continue;
+	if (text_content)
+		write(fd, text_content, len);
+	close(fd);
+	return (1);
+}
