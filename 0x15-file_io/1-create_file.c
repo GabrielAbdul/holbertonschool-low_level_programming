344b9ba231c1404302acaae49f5470fa9b485e33
gabrielabdul <gabrielabdul1@gmail.com>
2020-03-31 17:57:30 +0000

    creats an empty file if text_content is null

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,6 +23,8 @@ int create_file(const char *filename, char *text_content)
 		continue;
 	if (text_content)
 		write(fd, text_content, len);
+	if (!text_content)
+		write(fd, "", 0);
 	close(fd);
 	return (1);
 }
