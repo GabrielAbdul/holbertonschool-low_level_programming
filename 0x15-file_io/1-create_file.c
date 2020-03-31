579733fc6c80cbb5b4e96d37a32ffc84a87a92eb
GabrielAbdul <gabrielabdul1@gmail.com>
2020-03-31 19:01:55 +0000

    accounted for text_content == NULL by moving loop

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,10 +20,12 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (!fd)
 		return (-1);
-	for (; text_content[len]; len++)
-		continue;
 	if (text_content)
+	{
+		for (; text_content[len]; len++)
+			continue;
 		write_count = write(fd, text_content, len);
+	}
 	if (write_count == -1)
 		return (write_count);
 	close(fd);
