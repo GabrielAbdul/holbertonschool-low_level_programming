7c2a251ae36320dbc55e2bf1fc3feef3720c51f8
gabrielabdul <gabrielabdul1@gmail.com>
2020-05-30 01:59:44 +0000

    malloc space for hash_node_t * instead of hash_node_t

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,12 +14,23 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	if (!hash_table)
+	{
+		free(hash_table);
 		return (NULL);
+	}
+		
 	hash_table->size = size;
-	hash_table->array = malloc(sizeof(hash_node_t) * size);
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	
+	
 	if (!hash_table->array)
+	{
+		free(hash_table->array);
 		return (NULL);
+	}
+		
 	for (i = 0; i < size; i++)
 		hash_table->array[i] = (NULL);
+		
 	return (hash_table);
 }
