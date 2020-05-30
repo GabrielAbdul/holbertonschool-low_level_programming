13f0124ad98b60ee91a797c0d0d1d723cd8f2ae3
GabrielAbdul <gabrielabdul1@gmail.com>
2020-05-30 23:00:51 +0000

    initial commit

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+
+#define USAGE ("Usage: cp file_from file_to")
+#define SOURCE (argv[1])
+#define DEST (argv[2])
+#define NO_READ ("Error: Can't read from file %s\n")
+#define NO_WRITE ("Error: Can't write to %s\n")
+#define NO_CLOSE ("Error: Can't close fd %d\n")
+
+/**
+ * main - program that copies the content of a file to another file
+ *
+ * @argc: arg count
+ * @argv: argument vector
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	int source, dest, read_bytes, write_bytes;
+	char buf[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, USAGE), exit(97);
+
+	source = open(SOURCE, O_RDONLY);
+	if (!source)
+		dprintf(STDERR_FILENO, NO_READ), exit(98);
+	dest = open(DEST, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (!dest)
+		dprintf(STDERR_FILENO, NO_READ), exit(98);
+
+	while (read_bytes = read(source, buf, 1024) > 0)
+		write_bytes = write(dest, buf, read_bytes);
+
+	if (!read_bytes)
+		dprintf(STDERR_FILENO, NO_READ, SOURCE), exit(98);
+
+	if (!write_bytes)
+		dprintf(STDERR_FILENO, NO_WRITE, DEST), exit(99)
+
+	if (close(source) == -1)
+		dprintf(STDERR_FILENO, NO_CLOSE, source), exit(100);
+
+	if (close(dest) == -1)
+		dprintf(STDERR_FILENO, NO_CLOSE, dest), exit(100);
+	return (0);
+}
